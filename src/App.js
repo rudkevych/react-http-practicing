a3b77aefac05154cad6498839825821710176fb6
rudkevych <oksana.rudkevych@gmail.com>
2022-10-02 20:20:25 +0200

    feat: fetching movies data with fetch api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
 
+  const fetchMoviesHandler = () => {
+    fetch('http://swapi.dev/api/films')
+      .then((response) => response.json())
+      .then((data) => {
+        const transformedMovies = data.results.map((result) => {
+          return {
+            id: result.episode_id,
+            title: result.title,
+            releaseDate: result.release_date,
+            openingText: result.opening_crawl,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  };
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
