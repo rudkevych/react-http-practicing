4b0db4f5b031866f88dfc355116383a43b44a2f5
rudkevych <oksana.rudkevych@gmail.com>
2022-10-02 20:47:09 +0200

    feat: added loading handling

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch('http://swapi.dev/api/films');
     const data = await response.json();
     const transformedMovies = data.results.map((result) => {
@@ -19,6 +21,7 @@ function App() {
     });
 
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
   return (
     <React.Fragment>
@@ -26,7 +29,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {movies.length === 0 && !isLoading && <p>No movies found</p>}
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
